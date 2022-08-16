9a4f1edc2b6ba89cd0639aeca51f1a288547bbca
jasonsin88 <jason.c.sin@gmail.com>
2022-08-16 11:56:37 -0600

    established variables

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+function initPage() {
+    const inputEl = document.getElementById("city-input");
+    const searchEl = document.getElementById("search-button");
+    const clearEl = document.getElementById("clear-history");
+    const nameEl = document.getElementById("city-name");
+    const picEl = document.getElementById("current-pic");
+    const tempEl = document.getElementById("temperature");
+    const humidityEl = document.getElementById("humidity");
+    const windEl = document.getElementById("wind-speed");
+    const uvEl = document.getElementById("UV-index");
+    const historyEl = document.getElementById("history");
